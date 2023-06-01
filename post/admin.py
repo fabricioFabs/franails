9978f7fc6d3d5766a4c65e63af9d6bea19656053
fabricio <fabriciodbgtz@gmail.com>
2023-06-01 08:00:57 -0300

    fds

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+<<<<<<< HEAD
 from .models import Category, Post
+=======
+from .models import Category, Post, Services
+>>>>>>> 5592fd6 (initial commit)
 # Register your models here.
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -11,5 +15,17 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ['category']
     prepopulated_fields = {'slug': ('title',)}
 
+<<<<<<< HEAD
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
+=======
+class PostServices(admin.ModelAdmin):
+    list_display = ['title', 'category', 'updated_at', 'image_table']
+    list_filter = ['category']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Services, PostServices)
+
+>>>>>>> 5592fd6 (initial commit)
--- post/models.py
+++ post/models.py
@@ -38,4 +38,34 @@ class Post(models.Model):
         if self.image:
             return mark_safe('<img src="{}" height="50" />'.format(self.image.url))
         else:
+<<<<<<< HEAD
             return mark_safe('<p>Sem imagem</p>')        
+=======
+            return mark_safe('<p>Sem imagem</p>')        
+
+class Services(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+
+    title = models.CharField(max_length=150, null=False)
+    slug = models.SlugField(unique=True, null=False)  
+
+    image = models.ImageField(upload_to='images/', blank=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return str(self.item) + ": R$" + str(self.price)
+        
+    def __str__(self):
+        return self.title
+
+    def image_table(self):  
+        if self.image:
+            return mark_safe('<img src="{}" height="50" />'.format(self.image.url))
+        else:
+            return mark_safe('<p>Sem imagem</p>')     
+    
+    
+>>>>>>> 5592fd6 (initial commit)
